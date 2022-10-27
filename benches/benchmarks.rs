51fc721a53cde88f8c20b4cdc1742a45bc74e271
Daniel Hast <32797673+HastD@users.noreply.github.com>
2022-10-27 13:46:15 -0400

    added benchmark for unsatisfied_parity_checks

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -52,6 +52,24 @@ pub fn decoder_benchmarks(c: &mut Criterion) {
             BatchSize::SmallInput
         )
     });
+    c.bench_function("upc", |b| {
+        let (r, d, t) = (BLOCK_LENGTH as u32, BLOCK_WEIGHT as u32, ERROR_WEIGHT as u32);
+        let key_dist = random::get_key_dist();
+        let err_dist = random::get_err_dist();
+        let mut rng = random::get_rng();
+        let mut threshold_cache = ThresholdCache::with_parameters(r, d, t);
+        threshold_cache.precompute_all();
+        b.iter_batched_ref(
+            || {
+                let key = Key::random(&mut rng, &key_dist);
+                let e_supp = SparseErrorVector::random(&mut rng, &err_dist);
+                let syn = Syndrome::from_sparse(&key, &e_supp);
+                (key, syn)
+            },
+            |inputs| black_box(decoder::unsatisfied_parity_checks(&inputs.0, &mut inputs.1)),
+            BatchSize::SmallInput
+        )
+    });
 }
 
 criterion_group!(benches, decoder_benchmarks);
