3adc9d2343b20c052fcc0c278492c0b35517f575
Daniel Hast <32797673+HastD@users.noreply.github.com>
2023-01-24 21:43:26 -0500

    better compile-time error messages and macro design

--- bike_decoder/src/parameters.rs
+++ bike_decoder/src/parameters.rs
@@ -1,8 +1,8 @@
 // Can change these parameters at compile-time using environment variables
-pub const BLOCK_LENGTH: usize = env_usize_or!("BIKE_BLOCK_LENGTH", 587);
-pub const BLOCK_WEIGHT: usize = env_usize_or!("BIKE_BLOCK_WEIGHT", 15);
-pub const ERROR_WEIGHT: usize = env_usize_or!("BIKE_ERROR_WEIGHT", 18);
-pub const NB_ITER: usize = env_usize_or!("BIKE_NB_ITER", 7);
+pub const BLOCK_LENGTH: usize = env_or_usize!("BIKE_BLOCK_LENGTH", 587);
+pub const BLOCK_WEIGHT: usize = env_or_usize!("BIKE_BLOCK_WEIGHT", 15);
+pub const ERROR_WEIGHT: usize = env_or_usize!("BIKE_ERROR_WEIGHT", 18);
+pub const NB_ITER: usize = env_or_usize!("BIKE_NB_ITER", 7);
 
 pub const GRAY_THRESHOLD_DIFF: u8 = 3;
 
@@ -15,31 +15,37 @@ pub const SIZE_AVX: usize = ((BLOCK_LENGTH * 8) + (256 * 16 - 1)) / (256 * 16) *
 
 #[allow(dead_code, clippy::all)]
 const fn compile_time_assertions() {
-    const _: () = assert!(usize::BITS >= 32);
-    const _: () = assert!(0 < BLOCK_WEIGHT && BLOCK_WEIGHT < BLOCK_LENGTH);
-    const _: () = assert!(0 < ERROR_WEIGHT && ERROR_WEIGHT < BLOCK_LENGTH);
-    const _: () = assert!((BLOCK_WEIGHT + 1) / 2 + 1 <= u8::MAX as usize);
+    const _: () = assert!(usize::BITS >= 32, "16-bit systems not supported");
+    const _: () = assert!(
+        0 < BLOCK_WEIGHT && BLOCK_WEIGHT < BLOCK_LENGTH,
+        "BLOCK_WEIGHT must be positive and less than BLOCK_LENGTH"
+    );
+    const _: () = assert!(
+        0 < ERROR_WEIGHT && ERROR_WEIGHT < ROW_LENGTH,
+        "ERROR_WEIGHT must be positive and less than ROW_LENGTH"
+    );
+    const _: () = assert!(
+        (BLOCK_WEIGHT + 1) / 2 + 1 <= u8::MAX as usize,
+        "BLOCK_WEIGHT > 507 not supported"
+    );
     const _: () = assert!(BLOCK_LENGTH <= SIZE_AVX);
     const _: () = assert!(2 * SIZE_AVX <= u32::MAX as usize);
-    const _: () = assert!(NB_ITER >= 1);
+    const _: () = assert!(NB_ITER >= 1, "NB_ITER must be positive");
 }
 
-/// Parses environment variable as a usize if defined, otherwise yields the
-/// given default value. Causes a compilation error if the environment variable
-/// is defined but cannot be parsed.
-macro_rules! env_usize_or {
-    ( $name:literal, $default:literal $(,)? ) => {{
+/// Parses environment variable as `usize` if defined, otherwise yields the
+/// given `usize` value. Fails to compile if the environment variable is
+/// defined but cannot be parsed.
+macro_rules! env_or_usize {
+    ( $name:expr, $default:expr $(,)? ) => {{
         ::konst::result::unwrap_ctx!(::konst::option::unwrap_or!(
             ::konst::option::map!(::core::option_env!($name), ::konst::primitive::parse_usize),
-            ::core::result::Result::Ok::<
-                ::core::primitive::usize,
-                ::konst::primitive::ParseIntError,
-            >($default)
+            ::core::result::Result::Ok::<::core::primitive::usize, _>($default)
         ))
     }};
 }
 
-use env_usize_or;
+use env_or_usize;
 
 #[cfg(test)]
 mod tests {
@@ -47,7 +53,7 @@ mod tests {
 
     #[test]
     fn env_usize_default_value() {
-        const N: usize = env_usize_or!("ENV_USIZE_TEST_VAR", 42);
+        const N: usize = env_or_usize!("ENV_USIZE_TEST_VAR", 42);
         assert_eq!(N, 42);
     }
 }
